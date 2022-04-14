c7a4afa9ecd008dd39ff0f11bba9ce5626005437
Raimundo Moraga <raimundomoraga@hotmail.com>
2022-04-14 18:22:21 -0400

    initial commit

--- test.R
+++ test.R
@@ -1,7 +1,26 @@
 library(rnaturalearth)
 library(ggplot2)
 library(sp)
+library(plotly)
+library(ggmap)
+library(ggridges)
+
 glaciares <- ne_download(scale = 10, type = 'glaciated_areas', category = 'physical', returnclass = 'sf')
 
-ggplot(data=glaciares) + geom_sf() + coord_sf(xlim = c(-80, -60), ylim = c(-60,-5), expand = FALSE)
+latitudes = c(-56, -15)#y
+longitudes <- c(-78, -65) #x
 
+p <- ggplot(data=glaciares) + geom_sf() + coord_sf(xlim = longitudes, ylim = latitudes, expand = FALSE)
+ggplotly(p)
+p
+
+spatial_ramon <- SpatialPoints(coords=cbind(longitudes, latitudes), proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+# exploramos los limites
+spatial_ramon@bbox[4]
+
+# extraemos un mapa
+mapa <- get_map(location = spatial_ramon@bbox, maptype = "terrain", zoom = 7)
+
+# dibujamos el mapa
+ggmap(mapa)
