ff74601688b458a7f51986b2e0b78928e4a428d4
tcantuarias <tcantuarias@alumnos.uai.cl>
2022-04-18 20:48:12 -0400

    Update test_mapa_interactivo.R
    añadi centroides de glaciares como columnas a filtered glaciares

--- test_mapa_interactivo.R
+++ test_mapa_interactivo.R
@@ -7,49 +7,43 @@ library(ggmap)
 library(ggridges)
 pacman::p_load(tmap, sf, tidyverse, mapview, RColorBrewer, viridis,
                leafpop, leaflet, leaflet.extras, mapdeck, plotly,
-               ggiraph, widgetframe, maps)
+               ggiraph, widgetframe, st)
 
 glaciares <- ne_download(scale = 10, type = 'glaciated_areas', category = 'physical', returnclass = 'sf')
-#ciudades <- data(world.cities)
+pop<- ne_download(scale = 10, type = 'populated_places', category = 'cultural', returnclass='sf')
+df <- ne_countries(country = 'chile',returnclass = c("sf"))
+filtered_glaciares = st_filter(glaciares, df, .pred = st_intersects)
+filtered_ciudades = st_filter(pop, df, .pred = st_intersects)
+filtered_glaciares$name <-NULL
+filtered_glaciares <- mutate(filtered_glaciares, area = st_area(filtered_glaciares))
+
 
 latitudes = c(-56, -15)#y
 longitudes <- c(-78, -65) #x
 
-c <-ggplot(data = ciudades,geom_sf() + coord_sf(xlim = longitudes, ylim = latitudes, expand = FALSE)
-p <- ggplot(data=glaciares) + geom_sf() + coord_sf(xlim = longitudes, ylim = latitudes, expand = FALSE)
-ggplotly(p)
-p
+cent <- st_point_on_surface(filtered_glaciares)
+cent_temp <- unlist(cent$geometry)
+long <- cent_temp[seq(1, length(cent_temp),2)]
+lat <- cent_temp[-seq(1, length(cent_temp),2)]
 
-spatial_ramon <- SpatialPoints(coords=cbind(longitudes, latitudes), proj4string=CRS("+proj=longlat +datum=WGS84"))
-
-# exploramos los limites
-spatial_ramon@bbox[4]
-
-# extraemos un mapa
-mapa <- get_map(location = spatial_ramon@bbox, maptype = "terrain", zoom = 7)
+filtered_glaciares['cent_long'] <- long
+filtered_glaciares['cent_lat'] <- lat
 
 # dibujamos el mapa
 ggmap(mapa)
 
 pal <- viridis(n=length(unique(glaciares)),direction = 1)
 
-leaflet() %>%
-  addProviderTiles("OpenStreetMap",
-                   group = "OpenStreetMap"
-  )  %>%
-  addProviderTiles("Stamen.Terrain",
-                   group = "Stamen.Terrain"
-  ) %>% 
-  addPolygons(data = glaciares$geometry, 
+int_map <- filtered_glaciares %>% leaflet() %>% addProviderTiles("OpenStreetMap", group = "openstreetmap") %>%
+setView(lng = longitudes[2], lat = latitudes[2], zoom =10) %>% 
+  addPolygons(data = filtered_glaciares$geometry, 
               fillColor  = pal,
               fillOpacity = 0.8,
-              #popup = glaciares$featurecla
+              addMarkers(lng = filtered_glaciares$cent_long,lat = filtered_glaciares$cent_lat, popup="text"),
               color = "#FFFF00", 
-              weight = 1) %>% 
-  addLayersControl(
-    baseGroups = c(
-      "OpenStreetMap",
-      "Stamen.Terrain"),
-    position = "topleft"
-  )
+              weight = 1)
 
+
+
+int_map
+  
